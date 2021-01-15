d98ea01b73f9fcdf4a095c3c58b845dc6cf8da40
Tank <Piyush.Tank@CenturyLink.com>
2021-01-15 15:44:44 +0530

    logcommit

--- server/data.js
+++ server/data.js
@@ -1,7 +1,7 @@
 const users = [
   {
     _id: 1587912698986,
-    email: 'piyushtank20@gmail.com',
+    email: 'piyushtank@gmail.com',
     password: '$2b$13$CW3SP.VCKI0NQFgu9plKQeXc3KugDB9hD/EB.nDgS79FQyxzBSDBW',
   },
 ];
